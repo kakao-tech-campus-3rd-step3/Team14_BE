12c715d5b6800143b2b06a1070e38c1a117b9c9e
jinwon1234 <ljw0626@naver.com>
2025-08-18 22:01:06 +0900

    fix : SSR CORS 해결 완료

--- src/main/java/kakao/festapick/config/SecurityConfig.java
+++ src/main/java/kakao/festapick/config/SecurityConfig.java
@@ -82,7 +82,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of(frontDomain, "https://api.jinwon.click"));
+        configuration.setAllowedOriginPatterns(List.of(frontDomain));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
--- src/main/java/kakao/festapick/config/WebMvcConfig.java
+++ src/main/java/kakao/festapick/config/WebMvcConfig.java
@@ -14,7 +14,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins(frontDomain,  "https://api.jinwon.click")
+                .allowedOriginPatterns(frontDomain)
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowCredentials(true)
                 .allowedHeaders("*")
