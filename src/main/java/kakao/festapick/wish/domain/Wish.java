5b26e23f363fb1e5a174c9ea0215b59dff0a13f4
jyhyt1567 <jyhyt1567@gmail.com>
2025-08-21 17:42:13 +0900

    refactor: 패치 타입 lazy로 수정

--- src/main/java/kakao/festapick/wish/domain/Wish.java
+++ src/main/java/kakao/festapick/wish/domain/Wish.java
@@ -2,6 +2,7 @@ package kakao.festapick.wish.domain;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -22,12 +23,12 @@ public class Wish {
     @Column(name = "wish_id")
     private Long id;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @OnDelete(action = OnDeleteAction.CASCADE)
     @JoinColumn(name = "user_id", nullable = false)
     private UserEntity user;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @OnDelete(action = OnDeleteAction.CASCADE)
     @JoinColumn(name = "festival_id", nullable = false)
     private Festival festival;
