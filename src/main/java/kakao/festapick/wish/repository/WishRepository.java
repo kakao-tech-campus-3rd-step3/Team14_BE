187daa913670201e35628c0ecf22a2e634541004
jyhyt1567 <jyhyt1567@gmail.com>
2025-08-21 17:42:13 +0900

    refactor: 패치 타입 lazy로 수정

--- src/main/java/kakao/festapick/wish/repository/WishRepository.java
+++ src/main/java/kakao/festapick/wish/repository/WishRepository.java
@@ -3,12 +3,14 @@ package kakao.festapick.wish.repository;
 import java.util.List;
 import java.util.Optional;
 import kakao.festapick.wish.domain.Wish;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface WishRepository extends JpaRepository<Wish, Long> {
 
+    @EntityGraph(attributePaths = {"festival"})
     List<Wish> findByUserIdentifier(String identifier);
 
     Optional<Wish> findByUserIdentifierAndId(String identifier, Long id);
