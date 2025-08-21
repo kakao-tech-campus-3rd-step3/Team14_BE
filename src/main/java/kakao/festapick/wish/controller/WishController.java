bccb849e4b902d1214f6f0b894438e0405ece86e
jyhyt1567 <jyhyt1567@gmail.com>
2025-08-21 17:50:55 +0900

    remove: 사용하지 않는 requestDto 삭제

--- src/main/java/kakao/festapick/wish/controller/WishController.java
+++ src/main/java/kakao/festapick/wish/controller/WishController.java
@@ -1,7 +1,6 @@
 package kakao.festapick.wish.controller;
 
 import java.util.List;
-import kakao.festapick.wish.dto.WishRequestDto;
 import kakao.festapick.wish.dto.WishResponseDto;
 import kakao.festapick.wish.service.WishService;
 import lombok.RequiredArgsConstructor;
@@ -12,7 +11,6 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
