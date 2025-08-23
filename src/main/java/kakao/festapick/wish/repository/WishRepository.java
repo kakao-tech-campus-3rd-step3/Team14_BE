6967971b13fe1580a761d92870d7f4573822e4bd
jyhyt1567 <jyhyt1567@gmail.com>
2025-08-23 19:23:57 +0900

    refactor: EntityGraph 대신 JPQL fetch join 사용으로 inner join 사용하게 변경

--- src/main/java/kakao/festapick/wish/repository/WishRepository.java
+++ src/main/java/kakao/festapick/wish/repository/WishRepository.java
@@ -1,18 +1,18 @@
 package kakao.festapick.wish.repository;
 
-import java.util.List;
 import java.util.Optional;
 import kakao.festapick.wish.domain.Wish;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface WishRepository extends JpaRepository<Wish, Long> {
 
-    @EntityGraph(attributePaths = {"festival"})
+    @Query(value = "select w from Wish w join w.user u on u.identifier= :identifier join fetch w.festival f",
+            countQuery = "select count(w) from Wish w join w.user u on u.identifier= :identifier")
     Page<Wish> findByUserIdentifier(String identifier, Pageable pageable);
 
     Optional<Wish> findByUserIdentifierAndFestivalId(String identifier, Long festivalId);
