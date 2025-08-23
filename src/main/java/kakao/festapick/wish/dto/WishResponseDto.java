adb5f5f935c97da899fddbe44df47ad7f9bb34f0
jyhyt1567 <jyhyt1567@gmail.com>
2025-08-23 20:51:43 +0900

    refactor: WishResponseDto 반환 정보 변경

--- src/main/java/kakao/festapick/wish/dto/WishResponseDto.java
+++ src/main/java/kakao/festapick/wish/dto/WishResponseDto.java
@@ -2,6 +2,9 @@ package kakao.festapick.wish.dto;
 
 import kakao.festapick.festival.dto.FestivalDetailResponse;
 
-public record WishResponseDto(FestivalDetailResponse festivalResponseDto) {
+public record WishResponseDto(Long wishId,
+                              Long festivalId,
+                              String title,
+                              String areaCode) {
 
 }
