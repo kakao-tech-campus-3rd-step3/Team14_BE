8a2a086830970bbaf9afa7b7985a2dc983995bf8
jyhyt1567 <jyhyt1567@gmail.com>
2025-08-23 22:49:29 +0900

    test: 테스트 코드 작성

---
+++ src/test/java/kakao/festapick/wish/repository/WishRepositoryTest.java
@@ -0,0 +1,144 @@
+package kakao.festapick.wish.repository;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
+
+import java.util.Optional;
+import kakao.festapick.festival.domain.Festival;
+import kakao.festapick.festival.dto.FestivalRequestDto;
+import kakao.festapick.festival.repository.FestivalRepository;
+import kakao.festapick.user.domain.SocialType;
+import kakao.festapick.user.domain.UserEntity;
+import kakao.festapick.user.domain.UserRoleType;
+import kakao.festapick.user.repository.UserRepository;
+import kakao.festapick.wish.domain.Wish;
+import org.assertj.core.api.AssertionsForClassTypes;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.transaction.annotation.Transactional;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = Replace.NONE)
+@Transactional
+public class WishRepositoryTest {
+
+    private static final String identifier = "GOOGLE-1234";
+
+    private final FestivalRequestDto requestDto = new FestivalRequestDto("1234567", "test festival",
+            "11",
+            "test addr1", "test addr2", "http://asd.test.com/example.jpg", "20250823",
+            "20251231");
+
+    @Autowired
+    private WishRepository wishRepository;
+    @Autowired
+    private UserRepository userRepository;
+    @Autowired
+    private FestivalRepository festivalRepository;
+
+    private UserEntity saveUserEntity() {
+
+        return userRepository.save(new UserEntity(identifier,
+                "example@gmail.com", "exampleName", UserRoleType.USER, SocialType.GOOGLE));
+    }
+
+    private Festival saveFestival() {
+
+        String overView = "test overview";
+        String homepage = "http://asd.test.com";
+        return festivalRepository.save(new Festival(requestDto, overView, homepage));
+    }
+
+    @Test
+    @DisplayName("위시 등록 성공 테스트")
+    void createWishSuccess() {
+
+        UserEntity userEntity = saveUserEntity();
+
+        Festival festival = saveFestival();
+
+        Wish actual = wishRepository.save(new Wish(userEntity, festival));
+
+        assertAll(
+                () -> AssertionsForClassTypes.assertThat(actual.getId()).isNotNull(),
+                () -> AssertionsForClassTypes.assertThat(actual.getUser()).isEqualTo(userEntity),
+                () -> AssertionsForClassTypes.assertThat(actual.getFestival()).isEqualTo(festival)
+        );
+    }
+
+    @Test
+    @DisplayName("위시 아이디로 위시 찾기 성공 테스트")
+    void findWishByIdSuccess() {
+
+        UserEntity userEntity = saveUserEntity();
+
+        Festival festival = saveFestival();
+
+        Wish saved = wishRepository.save(new Wish(userEntity, festival));
+
+        Optional<Wish> find = wishRepository.findByUserIdentifierAndId(userEntity.getIdentifier(),
+                saved.getId());
+
+        assertThat(find).isPresent();
+
+        Wish actual = find.get();
+
+        assertAll(
+                () -> AssertionsForClassTypes.assertThat(actual.getId()).isNotNull(),
+                () -> AssertionsForClassTypes.assertThat(actual.getUser()).isEqualTo(userEntity),
+                () -> AssertionsForClassTypes.assertThat(actual.getFestival()).isEqualTo(festival)
+        );
+    }
+
+    @Test
+    @DisplayName("축제 아이디로 위시 찾기 성공 테스트")
+    void findWishByFestivalIdSuccess() {
+
+        UserEntity userEntity = saveUserEntity();
+
+        Festival festival = saveFestival();
+
+        wishRepository.save(new Wish(userEntity, festival));
+
+        Optional<Wish> find = wishRepository.findByUserIdentifierAndId(userEntity.getIdentifier(),
+                festival.getId());
+
+        assertThat(find).isPresent();
+
+        Wish actual = find.get();
+
+        assertAll(
+                () -> AssertionsForClassTypes.assertThat(actual.getId()).isNotNull(),
+                () -> AssertionsForClassTypes.assertThat(actual.getUser()).isEqualTo(userEntity),
+                () -> AssertionsForClassTypes.assertThat(actual.getFestival()).isEqualTo(festival)
+        );
+    }
+
+    @Test
+    @DisplayName("내 위치 리스트 조회 성공 테스트")
+    void findMyWishSuccess() {
+
+        UserEntity userEntity = saveUserEntity();
+
+        Festival festival = saveFestival();
+
+        wishRepository.save(new Wish(userEntity, festival));
+
+        Page<Wish> find = wishRepository.findByUserIdentifier(userEntity.getIdentifier(),
+                PageRequest.of(0, 1));
+
+        Wish actual = find.getContent().get(0);
+
+        assertAll(
+                () -> AssertionsForClassTypes.assertThat(actual.getId()).isNotNull(),
+                () -> AssertionsForClassTypes.assertThat(actual.getUser()).isEqualTo(userEntity),
+                () -> AssertionsForClassTypes.assertThat(actual.getFestival()).isEqualTo(festival)
+        );
+    }
+}
---
+++ src/test/java/kakao/festapick/wish/service/WishServiceTest.java
@@ -0,0 +1,127 @@
+package kakao.festapick.wish.service;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
+
+import java.lang.reflect.Field;
+import java.util.Optional;
+import kakao.festapick.festival.domain.Festival;
+import kakao.festapick.festival.dto.FestivalRequestDto;
+import kakao.festapick.festival.repository.FestivalRepository;
+import kakao.festapick.global.exception.DuplicateEntityException;
+import kakao.festapick.user.domain.SocialType;
+import kakao.festapick.user.domain.UserEntity;
+import kakao.festapick.user.domain.UserRoleType;
+import kakao.festapick.user.service.OAuth2UserService;
+import kakao.festapick.wish.domain.Wish;
+import kakao.festapick.wish.dto.WishResponseDto;
+import kakao.festapick.wish.repository.WishRepository;
+import org.assertj.core.api.AssertionsForClassTypes;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+public class WishServiceTest {
+
+    @InjectMocks
+    private WishService wishService;
+
+    @Mock
+    private OAuth2UserService oAuth2UserService;
+
+    @Mock
+    private FestivalRepository festivalRepository;
+
+    @Mock
+    private WishRepository wishRepository;
+
+    @Test
+    @DisplayName("위시 등록 성공")
+    void createWishSuccess() throws NoSuchFieldException, IllegalAccessException {
+        UserEntity user = testUser();
+        Festival festival = testFestival();
+        Wish wish = new Wish(1L, user, festival);
+
+        given(festivalRepository.findFestivalById(any()))
+                .willReturn(Optional.of(festival));
+        given(oAuth2UserService.findByIdentifier(any()))
+                .willReturn(user);
+        given(wishRepository.findByUserIdentifierAndFestivalId(any(), any()))
+                .willReturn(Optional.empty());
+        given(wishRepository.save(any()))
+                .willReturn(wish);
+
+        WishResponseDto responseDto = wishService.createWish(festival.getId(), user.getIdentifier());
+
+        assertAll(
+                () -> AssertionsForClassTypes.assertThat(responseDto.wishId()).isNotNull(),
+                () -> AssertionsForClassTypes.assertThat(responseDto.festivalId())
+                        .isEqualTo(festival.getId()),
+                () -> AssertionsForClassTypes.assertThat(responseDto.title())
+                        .isEqualTo(festival.getTitle()),
+                () -> AssertionsForClassTypes.assertThat(responseDto.areaCode())
+                        .isEqualTo(festival.getAreaCode())
+        );
+
+        verify(festivalRepository).findFestivalById(any());
+        verify(oAuth2UserService).findByIdentifier(any());
+        verify(wishRepository).findByUserIdentifierAndFestivalId(any(), any());
+        verify(wishRepository).save(any());
+        verifyNoMoreInteractions(festivalRepository);
+        verifyNoMoreInteractions(oAuth2UserService);
+        verifyNoMoreInteractions(wishRepository);
+    }
+
+    @Test
+    @DisplayName("중복으로 인한 위시 등록 실패")
+    void createWishFail() throws NoSuchFieldException, IllegalAccessException {
+        UserEntity user = testUser();
+        Festival festival = testFestival();
+        Wish wish = new Wish(1L, user, festival);
+
+        given(festivalRepository.findFestivalById(any()))
+                .willReturn(Optional.of(festival));
+        given(oAuth2UserService.findByIdentifier(any()))
+                .willReturn(user);
+        given(wishRepository.findByUserIdentifierAndFestivalId(any(), any()))
+                .willReturn(Optional.of(wish));
+
+        DuplicateEntityException e = Assertions.assertThrows(DuplicateEntityException.class,
+                () -> wishService.createWish(festival.getId(), user.getIdentifier()));
+        assertThat(e.getMessage()).isEqualTo("이미 좋아요한 축제입니다.");
+
+        verify(festivalRepository).findFestivalById(any());
+        verify(oAuth2UserService).findByIdentifier(any());
+        verify(wishRepository).findByUserIdentifierAndFestivalId(any(), any());
+        verifyNoMoreInteractions(festivalRepository);
+        verifyNoMoreInteractions(oAuth2UserService);
+        verifyNoMoreInteractions(wishRepository);
+    }
+
+    private Festival testFestival() throws NoSuchFieldException, IllegalAccessException {
+        FestivalRequestDto festivalRequestDto = new FestivalRequestDto("12345", "example title",
+                "11", "test area1", "test area2", "http://asd.example.com/test.jpg", "20250823",
+                "20251231");
+        Festival festival = new Festival(festivalRequestDto, "http://asd.example.com", "testtesttest");
+
+        Field idField = Festival.class.getDeclaredField("id");
+        idField.setAccessible(true);
+        idField.set(festival, 1L);
+
+        return festival;
+    }
+
+    private UserEntity testUser(){
+
+        return new UserEntity(1L, "KAKAO-1234567890", "asd@test.com", "testUser", UserRoleType.USER, SocialType.KAKAO);
+    }
+}
