f1e2d98549772c6b01ec74ca75a534c4670d8932
jinwon1234 <ljw0626@naver.com>
2025-09-08 16:55:51 +0900

    chore: CustomOAuthUser 역할을 enum 타입으로 관리

--- src/main/java/kakao/festapick/user/dto/CustomOAuth2User.java
+++ src/main/java/kakao/festapick/user/dto/CustomOAuth2User.java
@@ -1,6 +1,7 @@
 package kakao.festapick.user.dto;
 
 import kakao.festapick.user.domain.UserEntity;
+import kakao.festapick.user.domain.UserRoleType;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 
@@ -10,12 +11,12 @@ import java.util.Map;
 
 public class CustomOAuth2User implements OAuth2User {
 
-    private final String role;
+    private final UserRoleType userRoleType;
     private final String identifier;
     private final Map<String, Object> attributes;
 
     public CustomOAuth2User(Map<String,Object> attributes, UserEntity user) {
-        this.role = user.getRoleType().name();
+        this.userRoleType = user.getRoleType();
         this.identifier = user.getIdentifier();
         this.attributes = attributes;
     }
@@ -31,7 +32,7 @@ public class CustomOAuth2User implements OAuth2User {
         collection.add(new GrantedAuthority() {
             @Override
             public String getAuthority() {
-                return role;
+                return userRoleType.name();
             }
         });
         return collection;
