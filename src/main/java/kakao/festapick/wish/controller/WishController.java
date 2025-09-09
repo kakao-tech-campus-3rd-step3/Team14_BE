88416ebc88bb294eb1ed26f8e0299280f765c428
jyhyt1567 <jyhyt1567@gmail.com>
2025-09-09 17:26:47 +0900

    refactor: 페이지네이션 인자 값 제한 및 검증

--- src/main/java/kakao/festapick/wish/controller/WishController.java
+++ src/main/java/kakao/festapick/wish/controller/WishController.java
@@ -1,9 +1,11 @@
 package kakao.festapick.wish.controller;
 
+import jakarta.validation.constraints.Max;
 import kakao.festapick.wish.dto.WishResponseDto;
 import kakao.festapick.wish.service.WishService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
@@ -16,6 +18,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -37,10 +40,11 @@ public class WishController {
     @GetMapping("/wishes/my")
     public ResponseEntity<Page<WishResponseDto>> getWishes(
             @AuthenticationPrincipal String identifier,
-            @PageableDefault(page = 0, size = 10, sort = "id", direction = Sort.Direction.ASC)
-            Pageable pageable
+            @RequestParam(defaultValue = "0", required = false) int page,
+            @Max(value = 1000)
+            @RequestParam(defaultValue = "5", required = false) int size
             ) {
-        return new ResponseEntity<>(wishService.getWishes(identifier, pageable),
+        return new ResponseEntity<>(wishService.getWishes(identifier, PageRequest.of(page, size)),
                 HttpStatus.OK);
     }
 
