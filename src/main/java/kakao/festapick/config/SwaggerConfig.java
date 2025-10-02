159d895ad3fa796e0b3f8fec85a7ae2918f35eaa
YUNJAEGOONER <coygyj@gmail.com>
2025-10-02 20:04:22 +0900

    fix: Swagger config 설정 변경

--- src/main/java/kakao/festapick/config/SwaggerConfig.java
+++ src/main/java/kakao/festapick/config/SwaggerConfig.java
@@ -6,11 +6,15 @@ import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 import io.swagger.v3.oas.models.servers.Server;
 import java.util.List;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SwaggerConfig {
+
+    @Value("${spring.backend.domain}")
+    private String serverDomain;
 
     @Bean
     public OpenAPI openAPI(){
@@ -20,7 +24,7 @@ public class SwaggerConfig {
                         .version("v2.0.1"))
                 .servers(List.of(
                         new Server()
-                                .url("https://localhost:8080")
+                                .url(serverDomain)
                                 .description("개발용 서버")
                 ))
                 .components(new Components().addSecuritySchemes("JWT", new SecurityScheme()
