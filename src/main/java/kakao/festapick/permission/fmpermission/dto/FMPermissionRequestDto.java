822932f5a4a01756e3ec73a16ff9379f5de95354
jyhyt1567 <jyhyt1567@gmail.com>
2025-11-06 11:03:35 +0900

    fix: 축제 매니저 권한 요청 dto, 엔티티 사이즈 검증 추가

--- src/main/java/kakao/festapick/permission/fmpermission/dto/FMPermissionRequestDto.java
+++ src/main/java/kakao/festapick/permission/fmpermission/dto/FMPermissionRequestDto.java
@@ -1,6 +1,8 @@
 package kakao.festapick.permission.fmpermission.dto;
 
 import jakarta.validation.Valid;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import java.util.List;
@@ -8,6 +10,8 @@ import kakao.festapick.fileupload.dto.FileUploadRequest;
 
 public record FMPermissionRequestDto(
         @NotBlank(message = "소속 기관 입력은 필수입니다.")
+        @Max(value = 50, message = "소속 기관은 최대 50자 입력가능합니다.")
+        @Min(value = 2, message = "소속 기관은 최소 2자 이상 입력해야합니다.")
         String department,
 
         @Size(min = 1,message = "최소 1개 이상의 서류를 업로드해야합니다.")
