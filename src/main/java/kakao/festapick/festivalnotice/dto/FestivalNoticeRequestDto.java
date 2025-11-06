a186a40803919d218003e4c72181a6c3eab4f6e8
jyhyt1567 <jyhyt1567@gmail.com>
2025-11-06 10:52:07 +0900

    fix: 축제 공지 요청 dto, 엔티티 사이즈 검증 추가

--- src/main/java/kakao/festapick/festivalnotice/dto/FestivalNoticeRequestDto.java
+++ src/main/java/kakao/festapick/festivalnotice/dto/FestivalNoticeRequestDto.java
@@ -1,17 +1,22 @@
 package kakao.festapick.festivalnotice.dto;
 
 import jakarta.validation.Valid;
+import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import java.util.List;
 import kakao.festapick.fileupload.dto.FileUploadRequest;
 
 public record FestivalNoticeRequestDto(
         @NotBlank
+        @Max(value = 100, message = "공지사항 제목은 최대 100자입니다.")
         String title,
 
         @NotBlank
+        @Max(value = 2000, message = "공지사항 내용은 최대 2000자입니다.")
         String content,
 
+        @Size(max = 10, message = "공지사항 사진은 최대 10장 업로드 가능합니다.")
         List<@Valid FileUploadRequest> images
 ) {
 }
