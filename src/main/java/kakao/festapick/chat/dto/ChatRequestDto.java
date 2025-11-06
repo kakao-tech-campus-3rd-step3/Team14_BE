fec10597153606a7a1f4f0f36b48168a14932d4c
jyhyt1567 <jyhyt1567@gmail.com>
2025-11-06 10:18:55 +0900

    fix: 채팅 요청 dto 사이즈 검증 추가

--- src/main/java/kakao/festapick/chat/dto/ChatRequestDto.java
+++ src/main/java/kakao/festapick/chat/dto/ChatRequestDto.java
@@ -1,10 +1,12 @@
 package kakao.festapick.chat.dto;
 
+import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.NotBlank;
 import kakao.festapick.fileupload.dto.FileUploadRequest;
 
 public record ChatRequestDto(
         @NotBlank
+        @Max(value = 255, message = "채팅은 최대 255자 까지 작성 가능합니다.")
         String content,
         FileUploadRequest imageInfo
 ) {
